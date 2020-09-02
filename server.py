b59417347955c801a6be50e651adf78d4cf99bce
bear <bear@lenovo.mskcc.org>
2020-09-01 23:20:45 -0400

    update: Added 'Hit' logic

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ deck = Deck(); deck.shuffle()
 players = []
 connections = []
 
-def threaded_client(conn):
+def threaded_client(conn, curPlayer):
     # send message to the player.
     conn.send(str.encode('Welcome to Blackjack! Please input your name:'))
     while True:
@@ -25,7 +25,11 @@ def threaded_client(conn):
                 print("No message from player, remove it")
                 if conn in connections: connections.remove(conn)
             elif player_msg == 'Hit':
-                pass
+                player = players[curPlayer]
+                player.draw(deck)
+                hand = player.getHand()
+                print('{} got new card, current hand: {}'.format(player.name, hand))
+                conn.send(str.encode('Your current hand: {}\n'.format(hand)))
             elif player_msg == 'Stand':
                 pass
             else: # player input name
@@ -33,19 +37,22 @@ def threaded_client(conn):
                 player.draw(deck, 2)
                 players.append(player)
                 hand = player.getHand()
-                print('{} joined and got two cards: {}'.format(player_msg, str(hand)))
+                print('{} joined and got two cards: {}'.format(player_msg, hand))
+                conn.send(str.encode('Your current hand: {}\n'.format(hand)))
+                conn.send(str.encode('Hit or Stand: '))
         except: continue
 
     # close the connection with the player
     # conn.close()
     # if conn in connections: connections.remove(conn)
 
+curPlayer = 0
 while True:
     # establish connection with client.
     conn, addr = s.accept()
     print("{} is connected".format(addr[0]))
     connections.append(conn)
-    start_new_thread(threaded_client, (conn,))
-
+    start_new_thread(threaded_client, (conn, curPlayer))
+    curPlayer += 1
 #conn.close()
 #s.close()
