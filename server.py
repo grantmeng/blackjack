6dba48ca447b90e4cc75ce8bb701c0eb2e55410d
Xiang <savagemango69@gmail.com>
2020-09-17 18:34:35 -0400

    feat: switch to flask

--- server.py
+++ server.py
@@ -1,82 +1,20 @@
 from config import *
 from blackjackclasses import *
-import socket
-from _thread import *
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try: s.bind((SERVER, PORT))
-except socket.error as e: print(e)
+@app.route('/')
+def index():
+    return redirect(url_for('static', filename='login.html'))
 
-s.listen(MAX_PLAYERS)
-print("Waiting for player to join")
+@app.route('/login', methods=['POST'])
+def login():
+    user = request.form['name']
+    return redirect(url_for('success', name=user))
 
-deck = Deck(); deck.shuffle()
-players = [None] * MAX_PLAYERS
-connections = [None] * MAX_PLAYERS
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
 
-def broadcast(curConn, msg):
-    for conn in connections:
-        if conn and conn != curConn:
-            conn.sendall(str.encode(msg))
-
-def threaded_client(conn, curPlayer):
-    # send message to the player and ask player to input name
-    while True: 
-        conn.sendall(str.encode('Player {}: welcome to Blackjack! Please input your name.'.format(curPlayer)))
-        player_msg = conn.recv(2048).decode()
-        if not player_msg: continue
-        players[curPlayer] = Player(player_msg)
-        break
-    
-    # the player created, initially assign two cards
-    player = players[curPlayer]
-    player.draw(deck, 2)
-    hand = player.getHand()
-    print('Player {}: {} joined and got two cards: {}'.format(curPlayer, player_msg, hand))
-    points = player.getPoints()
-    if points == float('inf'):
-        print('Player {}: {} won with blackjack!: {}'.format(curPlayer, player.name, hand))
-        conn.sendall(str.encode('Player {}: you won with blackjack!: {}\n'.format(curPlayer, hand)))
-    else:
-        conn.sendall(str.encode('Player {}: your current hand: {}\n'.format(curPlayer, hand)))
-        conn.sendall(str.encode('Player {}: Hit or Stand?\n'.format(curPlayer)))
-
-    # continue the game       
-    while True:
-        #try:
-        player_msg = conn.recv(2048).decode()
-        if player_msg == 'Hit':
-            new_hand = player.draw(deck)
-            hand = player.getHand()
-            print('Player {}: {} got new card, current hand: {}'.format(curPlayer, player.name, hand))
-            msg = []
-            for card in new_hand:
-                msg.append(card.get())
-            msg = '{} got new card: {}\n'.format(player.name, ' '.join(msg))
-            broadcast(conn, msg)
-            points = player.getPoints()
-            if points == float('-inf'):
-                print('Player {}: {} bursted with hand: {}'.format(curPlayer, player.name, hand))
-                conn.send(str.encode('Player {}, you bursted with hand: {}\n'.format(curPlayer, hand)))
-            else:
-                conn.send(str.encode('Player {}, your current hand: {}\n'.format(curPlayer, hand)))
-        elif player_msg == 'Stand':
-            broadcast(conn, '{} standed\n'.format(player.name))
-            conn.send(str.encode('Player {}, your current hand: {}\n'.format(curPlayer, hand)))
-        else: pass
-        #except: continue
-
-    # close the connection with the player
-    # conn.close()
-    # if conn in connections: connections.remove(conn)
-
-curPlayer = 0
-while True:
-    # establish connection with client.
-    conn, addr = s.accept()
-    print("{} is connected".format(addr[0]))
-    connections[curPlayer] = conn
-    start_new_thread(threaded_client, (conn, curPlayer))
-    curPlayer += 1
-#conn.close()
-#s.close()
+if __name__ == '__main__':
+   app.run(SERVER, PORT, DEBUG)
