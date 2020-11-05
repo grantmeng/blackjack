af777634b2d22d63a949cf6a4899b79dbc26927b
Grant Meng <grantmeng@gmail.com>
2020-11-05 16:31:32 -0500

    final update

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,5 +3,4 @@ import json
 def test_index(app, client):
     res = client.get('/')
     assert res.status_code == 200
-    expected = {'hello': 'world'}
-    #assert expected == json.loads(res.get_data(as_text=True))
+    assert b'Welcome to Blackjack!' in res.data
